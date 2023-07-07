cf4c33a3c8ac91e83da69f5b181aaffc8fa9aab1
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-07-07 20:18:38 +0200

    Update front page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-2 d-flex">
+                <div class="card w-100">
+                    <div class="card-header">Reklama</div>
+                    <div class="card-body">
+                        <h4>I am an ad</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Ogłoszenia</div>
+                    <div class="card-body">
+                        <p>Witaj na moim blogu!</p>
+                        <p>Cieszę się, że tu jesteś. Ten blog jest dedykowany światu gier, w którym podzielę się swoimi
+                            spostrzeżeniami, recenzjami i ciekawostkami z tego fascynującego świata.</p>
+                        <p>Przygotuj się na podróż pełną emocji, nowych odkryć i pasji, które łączą nas wszystkich jako
+                            graczy. Będę regularnie aktualizować treści, więc miej oczy szeroko otwarte!</p>
+                        <p>Zapraszam do zapoznania się z moimi wpisami i komentowania, abyśmy mogli wspólnie dzielić się
+                            naszymi opiniami i doświadczeniami. Mam nadzieję, że znajdziesz tutaj wiele inspirujących treści
+                            i że spędzisz tu miło czas.</p>
+                        <p>Dziękuję jeszcze raz za odwiedziny i życzę udanej przygody w świecie gier!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <div class="card-header">Statystyki strony</div>
+                    <div class="card-body">
+                        <p>Odwiedzin: N/A</p>
+                        <p>Postów: N/A</p>
+                        <p>Komentarzy: N/A</p>
+                        <p>Wyświetleń postów: N/A</p>
+                        <p>Użytkowników: N/A</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12" style="margin-top: 20px;">
+                <div class="card">
+                    <div class="card-header">Posty</div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach ($posts as $post)
+                                <li class="list-group-item">
+                                    <a href="#">{{ $post->title }}</a> - {{ $post->updated_at }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('homepage');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 
 // Posts
 Route::get('/posts', [App\Http\Controllers\PostController::class, 'index'])->name('posts.index');

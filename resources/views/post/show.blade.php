7d7943a5e1b68c467e90989f96accbbf30ae676f
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-07-07 20:36:02 +0200

    Create add comment form on post

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -10,18 +10,49 @@
                         <p class="card-text">
                             Created at: <strong>{{ $post->created_at->format('d.m.Y H:i') }}</strong>
                             Last update: <strong>{{ $post->updated_at->format('d.m.Y H:i') }}</strong>
+                            <div class="d-flex">
+                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning mr-2">Edytuj</a>
+                                <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Usuń</button>
+                                </form>
+                            </div>
                         </p>
+
                         <hr>
                         <p class="card-text">{{ $post->content }}</p>
-                        <div class="d-flex">
-                            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary mr-2">Wyświetl</a>
-                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning mr-2">Edytuj</a>
-                            <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Usuń</button>
-                            </form>
-                        </div>
+
+
+                        @auth
+                            <div class="card border">
+                                <div class="card-body">
+                                    <form action="">
+                                        <div class="mb-3 row">
+                                            <label for="comment" class="col-md-4 col-form-label text-md-end">Zamieść
+                                                komentarz</label>
+                                            <div class="col-md-6">
+                                                <textarea id="comment" type="text" class="form-control @error('comment') is-invalid @enderror" name="comment"
+                                                    required autocomplete="comment" autofocus></textarea>
+                                                @error('comment')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="mb-0 row">
+                                            <div class="col-md-6 offset-md-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    Zamieść
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        @endauth
+                        
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('homepage');
+Route::get('/home', function() {
+    return redirect(route('homepage'));
+});
 
 Auth::routes();
 

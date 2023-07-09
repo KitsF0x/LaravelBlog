9510bb0dce7c90479310037e7745ec97ee8e6900
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-07-09 12:55:21 +0200

    Add admin label when user's role is admin next to the username in navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
+                                    @if(Auth::user()->role == App\Enums\UserRole::ADMIN)
+                                    <strong>({{Auth::user()->role }})</strong>
+                                    @endif
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
